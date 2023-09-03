a5899416d2e849547b1d5e794e01a54a82d24ac4
Giuliano Dominici <giulianodominici@MacBook-Pro-de-Giuliano.local>
2023-09-03 16:34:30 +0200

    plug_manager

---
+++ init.lua
@@ -0,0 +1,4 @@
+require("config")
+require('settings')
+require('keymaps')
+vim.cmd[[colorscheme tokyonight-moon]]
---
+++ lua/settings.lua
@@ -0,0 +1,15 @@
+--vim.g.loaded_netrw=1
+--vim.g.loaded_netrwPlugin=1
+vim.g.nvim_tree_disable_netrw = 0
+vim.opt.termguicolors=true
+vim.cmd[[let g:ruby_host_prog="/usr/bin/neovim-ruby-host.ruby2.4"]]
+vim.cmd[[set splitright]]
+vim.cmd[[set splitbelow]]
+vim.cmd[[set noswapfile]]
+vim.cmd[[set number]]
+vim.cmd[[set relativenumber]]
+vim.cmd[[set tabstop=4]]
+vim.cmd[[set shiftwidth=4]]
+vim.cmd[[set scrolloff=5]]
+vim.cmd[[set scrollopt=ver]]
+vim.cmd[[set path+=**]]
